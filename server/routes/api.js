484bc1f79ee559a526d5473eb85eadd41a01f915
Craig <craig.b.macgregor@gmail.com>
2017-05-14 17:07:55 +1200

    rpc api setup and unit tested

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,25 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const Client = require('bitcoin-core');
+const express = require('express')
+const router = express.Router()
+const Rpc = require('../lib/rpc')
 
-const navClient = new Client({
-  username: "navpi", //RPC Username
-  password: "securepass", //RPC Password
-  port: 44444,
-  host: "127.0.0.1",
-})
+router.get('/', (req, res) => res.send('api works'))
+router.post('/navcoin/rpc', Rpc.navcoinRpc)
 
-/* GET api listing. */
-router.get('/', (req, res) => {
-  res.send('api works');
-});
-
-router.get('/navcoin/getinfo', (req, res) => {
-  navClient.getInfo().then((info) => {
-    console.log(info);
-    res.send(info);
-  });
-
-});
-
-module.exports = router;
+module.exports = router
