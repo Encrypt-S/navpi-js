3c6f44ee77162526a8ead1cc24d956c6a8fbf02a
Paul Sanderson <paulsanderson@me.com>
2017-10-25 17:34:08 +1300

    Adds app routing module

---
+++ navpi-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot([
+
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+      /* define app module routes here, e.g., to lazily load a module
+       (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
+       */
+      // { path: '', component: HomeComponent },
+      // { path: 'callback', component: CallbackComponent },
+      // { path: '**', redirectTo: '' }
+    ])
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
