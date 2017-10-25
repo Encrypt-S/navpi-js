41d34fa7381b705448a6e20d09c905a4ad8b60a6
Paul Sanderson <paulsanderson@me.com>
2017-10-25 17:36:13 +1300

    include the dashboard and routing module into the application

--- navpi-frontend/src/app/app.module.ts
+++ navpi-frontend/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardModule} from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // Applicaiton Modules
+    DashboardModule,
+
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
