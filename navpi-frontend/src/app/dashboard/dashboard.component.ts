c68a3f4f0e6ac73e021a233b49655d4d3f021aff
Paul Sanderson <paulsanderson@me.com>
2017-10-26 14:00:24 +1300

    removes trading view graphs

--- navpi-frontend/src/app/dashboard/dashboard.component.ts
+++ navpi-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+// declare var TradingView;
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,6 +12,29 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // new TradingView.MediumWidget({
+    //   "container_id": "tv-medium-widget-54b92",
+    //   "symbols": [
+    //     [
+    //       "USD",
+    //       "BITTREX:NAVUSD|1y"
+    //     ],
+    //     [
+    //       "Bitcoin",
+    //       "BITTREX:NAVBTC|1y"
+    //     ]
+    //   ],
+    //   "greyText": "Quotes by",
+    //   "gridLineColor": "#e9e9ea",
+    //   "fontColor": "#83888D",
+    //   "underLineColor": "rgba(152, 0, 255, 0.29)",
+    //   "trendLineColor": "rgba(152, 0, 255, 1)",
+    //   "width": "100%",
+    //   "height": "400px",
+    //   "locale": "en"
+    // });
+
   }
 
 }
--- navpi-frontend/src/app/dashboard/dashboard.module.ts
+++ navpi-frontend/src/app/dashboard/dashboard.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {SharedMaterialModule} from '../shared-ui/material/shared-material.module';
+import {SharedUIModule} from '../shared-ui/ui/shared-ui.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
 
+
     SharedMaterialModule,
+    SharedUIModule,
 
     DashboardRoutingModule
   ],
