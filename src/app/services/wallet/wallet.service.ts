2511ccad6bfbf34e49a215ab4ed868de4d4cc578
Paul Sanderson <paul@creativetech.io>
2017-11-14 19:33:01 +1300

    Adds wallet service

---
+++ src/app/services/wallet/wallet.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+
+
+@Injectable()
+export class WalletService {
+
+  constructor(
+    private http: Http
+
+  ) { }
+
+  getStakingReport() {
+
+
+  }
+
+}
