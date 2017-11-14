5d8f431487d5b9a360732c513dbb0a762bc470c6
Paul Sanderson <paul@creativetech.io>
2017-11-14 20:42:37 +1300

    Adds staking vo

---
+++ src/app/services/wallet/vo/staking.vo.ts
@@ -0,0 +1,23 @@
+export interface StakingVO {
+
+
+  graph: Staking30DayVO[];
+
+  hr24: number;
+  day7: number;
+  day30: number;
+  day365: number;
+  lastestStake: number;
+  latestTime: Date;
+  stakeCounted: number;
+  timeTaken: number;
+
+}
+
+
+export interface Staking30DayVO {
+
+  date: Date;
+  amount: number
+
+}
