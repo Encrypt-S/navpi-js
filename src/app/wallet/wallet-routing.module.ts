7dc6b1338a399055d877a4dbbd12ce4de243f0da
mike d <202134+rex-kwon@users.noreply.github.com>
2018-04-04 22:18:10 +1200

    cleans up wallet code

--- src/app/wallet/wallet-routing.module.ts
+++ src/app/wallet/wallet-routing.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {WalletComponent} from './wallet.component';
 
-
 @NgModule({
   imports: [
     RouterModule.forChild([
--- src/app/wallet/wallet.module.ts
+++ src/app/wallet/wallet.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { WalletComponent } from './wallet.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {WalletComponent} from './wallet.component';
 import {WalletRoutingModule} from './wallet-routing.module';
 import {SharedMaterialModule} from '../shared-ui/material/shared-material.module';
 import {SharedUIModule} from '../shared-ui/ui/shared-ui.module';
