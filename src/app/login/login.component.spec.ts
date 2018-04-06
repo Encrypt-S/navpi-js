a63176e1adb460b79cb797d72045caebafaa466f
red010b37 <paul@creativetech.io>
2018-04-06 18:31:57 +1200

    Fixes login component tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
+import {LoginServiceStub} from "../services/login/login.service.stub";
+import {LoginService} from "../services/login/login.service";
+import {ReactiveFormsModule} from "@angular/forms";
+import {SharedMaterialModule} from "../shared-ui/material/shared-material.module";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +12,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        SharedMaterialModule,
+      ],
+      providers: [
+        {provide: LoginService, useClass: LoginServiceStub}
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
