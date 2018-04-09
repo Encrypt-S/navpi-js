2f230043351fb35c39fbd8cc2e3bd14a6f3b7820
Paul Sanderson <paul@creativetech.io>
2018-04-09 12:33:16 +1200

    Updates Error service to display snackbar

--- src/app/services/error/error.service.ts
+++ src/app/services/error/error.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpErrorResponse} from '@angular/common/http';
+import {MatSnackBar} from '@angular/material';
 
 @Injectable()
 export class ErrorService {
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) { }
 
   UnknownHTTPError(e: HttpErrorResponse) {
     console.warn('ErrorService: UnknownHTTPError: not implemented');
+
+    this._snackBar.open('Error connecting to the server - please try agin', 'Ok');
+
   }
 
 
