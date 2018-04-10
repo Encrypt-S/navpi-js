3891cf86c5d366db6660f33d676040dcefdb116e
red010b37 <paul@creativetech.io>
2018-04-11 00:05:47 +1200

    Fixes Imports

--- src/app/shared/services/guards/auth.guard.spec.ts
+++ src/app/shared/services/guards/auth.guard.spec.ts
@@ -1,7 +1,7 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import {async, inject, TestBed} from '@angular/core/testing';
 
-import { AuthGuard } from './auth.guard';
-import {RouterTestingModule, } from "@angular/router/testing";
+import {AuthGuard} from './auth.guard';
+import {RouterTestingModule,} from "@angular/router/testing";
 import {AuthService} from "../auth/auth.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {CoreService} from "../core/core.service";
